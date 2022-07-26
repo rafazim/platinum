18133086a4bb30311f57cc05cbfbb62a51f0e3c6
Rafael Zim <rafael_zim@hotmail.com>
2022-07-26 20:13:48 -0300

    Header almost done

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,16 +1,36 @@
 import icon from '../../assets/img/Platinum.png';
 import ZapLink from '../ZapLink';
+import { BASE_URL } from '../../utils/request';
+import insta from '../../assets/img/download.jpg';
 import './styles.css';
 
 function Header() {
     return (
         <header>
+
             <div className="platinum-logo-container">
-                <img src={icon} alt="Platinum" />
+                <div className="platinum-logo">
+                    <img src={icon} alt="Platinum" />
+                </div>
+                <h1>PLATINUM CONTABILIDADE</h1>
             </div>
-            <div className='platinum-zaplink'>
-                <ZapLink />
+
+            <menu className='platinum-menu'>
+                <div className="platinum-menu-inicio">
+                    <a href={`${ BASE_URL }/index.html`} target={"_blank"} rel="noopener noreferrer"><text>Início</text></a>
+                </div>
+            </menu>
+
+
+            <div className='platinum-social-media'>
+                <div className="platinum-social-media-instagram">
+                    <a href='https://www.instagram.com/platinumcontabil/' target={"_blank"} rel="noopener noreferrer"><img src={insta} alt="Instagram" /></a>
+                </div>
+                <div className="platinum-social-media-zaplink">
+                    <ZapLink />
+                </div>
             </div>
+
         </header>
     )
 }
--- frontend/src/components/ZapLink/index.tsx
+++ frontend/src/components/ZapLink/index.tsx
@@ -4,8 +4,7 @@ import './styles.css';
 function ZapLink() {
     return (
         <div className="platinum-zaplink">
-            <img src={icon} alt="Contato" />
-            <a href="https://api.whatsapp.com/send?phone=5511997126332&text=Platinum%20Contabil"></a>
+            <a href="https://api.whatsapp.com/send?phone=5511997126332&text=Platinum%20Contabil"target={"_blank"} rel="noopener noreferrer"><img src={icon} alt="Contato" /></a>
         </div>
     )
 }
